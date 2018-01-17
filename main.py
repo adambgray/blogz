c090492f6d832acf320ce0ae84cd3bed7703b20d
Adam Gray <adambgray@gmail.com>
2018-01-16 19:29:53 -0500

    created main.py, imported and configured local database

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/flicklistbuild-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+
