29320efe64a9085a35b2d22c580d6cb4d93a7971
Adam Gray <adambgray@gmail.com>
2018-01-24 23:38:59 -0500

    added index.html and before_request handler

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Blog(db.Model):
 
@@ -14,7 +15,7 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.owner = owner
@@ -26,21 +27,33 @@ class User(db.Model):
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
 def logged_in_user():
-    owner = User.query.filter_by(email=session['user']).first()
+    owner = User.query.filter_by(username=session['username']).first()
     return owner
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'index', 'blog']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
 
 @app.route('/')
 def index():
+    users = User.query.all()
+    return render_template('index.html',page_title="Blog Users", users=users)
 
-    return render_template('base.html',page_title="The Blog")
-
-@app.route('/blog')
+@app.route('/blog', methods=['POST', 'GET'])
 def blog():
-    title = request.args.get('title')
-    body = request.args.get('body')
-    if title and body:
-        return render_template('blogentry.html', title=title, body=body)
+    if request.method =='POST':
+        title = request.args.get('title')
+        body = request.args.get('body')
+        if title and body:
+            return render_template('blogentry.html', title=title, body=body)
     else:
         blogs = Blog.query.all()
         return render_template('blog.html',page_title="The Blog", 
@@ -62,6 +75,67 @@ def newpost():
             return render_template('newpost.html', page_title="Add a new post", title=title, body=body, error='Blog must contain a title and a body.')
     else:
         return render_template('newpost.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/newpost')
+        
+        elif not user:
+            flash('User does not exist', 'error')
+
+        else:
+            flash('Incorrect password', 'error')
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if len(username) < 3 or len(username) > 20:
+            flash('Username must be between 3 and 20 characters with no spaces.', 'error')
+        
+        if ' ' in username:
+            flash('Username must not contain spaces.', 'error')
+
+        if password != verify:
+            flash('Password and confirmation must match', 'error')
+        
+        if len(password) <3 or len(password) > 20:
+            flash('Password must be between 3 and 20 characters with no spaces.', 'error')
+        
+        if ' ' in password:
+            flash('Password must not contain spaces.', 'error')
+
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/')
+        else:
+            # TODO - user better response messaging
+            flash('Duplicate user', 'error')
+
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/blog')
 
 
 
